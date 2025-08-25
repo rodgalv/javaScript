7abce408e8804847954806469a91f8b6de52893e
Rodrigo Galvao <rodrigogalvaobs@gmail.com>
2025-08-25 18:45:50 +0200

    first commit

---
+++ built-in functions/math/script.js
@@ -0,0 +1,15 @@
+let maior = Math.max(6,12,28,55,8);
+
+console.log(maior);
+
+let menor = Math.min(6,12,28,55,8);
+
+console.log(menor);
+
+let arredondar = Math.round(5.29875);
+
+console.log(arredondar);
+
+let arredondarParaCima = Math.ceil(5.29875);
+
+console.log(arredondarParaCima);
---
+++ built-in functions/prompt/script.js
@@ -0,0 +1,6 @@
+let idade = prompt("Qual é a sua idade?");
+
+console.log(idade);
+
+let nome = prompt ("Qual o seu nome?");
+console.log(`O seu nome é ${nome}`);
